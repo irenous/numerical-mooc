2b80d12fb5f8205d1aec18367a442ac0afff0bac
lpetre <lpetre@ulb.ac.be>
2015-10-11 15:57:16 +0200

    Utilisation de numpy.

--- working/rocket/rocket.py
+++ working/rocket/rocket.py
@@ -1,46 +1,54 @@
 import numpy as npy
 import matplotlib.pyplot as plt
 
+def f(t, u, ms = 50.0, g = -9.81, rho = 1.019, r = 0.5, ve = 325, CD = 0.15, mpdot = 20):
+    u_ = npy.zeros(3)
 
-def func(t, v, mp0):
-    mp = mp0 - t*20
-    if mp < 0 :
-        mp = 0
-
-    v_ = -9.81 - (0.5*1.091*npy.pi*0.25*0.15*v*npy.abs(v))/(50+mp)
+    # Mass u_[0]
     if t < 5 :
-        v_ = v_ +(20*325)/(50+mp)
+        u_[0] = -mpdot
+    else :
+        u_[0] = 0
+    
+    # Height u_[1]
+    u_[1] = u[2]
 
-    return v_
+    # Speed u_[2]
+    u_[2] = g - (0.5*rho*npy.pi*(r**2)*CD*u[2]*npy.abs(u[2]))/(ms+u[0])
+    if t < 5 :
+        u_[2] = u_[2] + (mpdot*ve)/(ms+u[0])
 
-def stop(h, v, step):
-    h.append(h[-1] + v[-1]*step) #v[-1] ou v[-2] ? : on prend la vitesse de cette itération est celle d'avant ?
+    return u_
 
-    return (h[-1] < 0)
+def stop(t, u, step):
+    return (u[1, step] < 0)
 
-def solve(x, y, f_func, stop_func, dx):
+def solve(t, u, f_func, stop_func, dt):
     i = 0
     while True :
-        x.append(x[i] + dx)
-        y.append(y[i] + dx*f_func(x[i], y[i]))
+        t[i+1] = t[i] + dt
+        u[:,i+1] = u[:,i] + dt*f_func(t[i], u[:,i])
 
-        if stop_func(x, y, i) :
+        if stop_func(t, u, i) :
             break
 
         i = i + 1
 
 if __name__ == "__main__":
-    t = [0]
-    h = [0]
-    v = [0]
+    t = npy.linspace(0, 40, 4000)
+    u = npy.zeros((3,4000))
+    u[0, 0] = 100
 
-    solve(t, v, lambda x, y: func(x, y, 100), lambda x, y, i: stop(h, y, 0.001), 0.001)
+    solve(t, u, lambda t, u: f(t, u), lambda t, u, i: stop(t, u, i), 0.01)
 
-    print("Temps et vitesse du retour au sol :", t[-1], v[-1])
-    print("Hauteur max et temps :", max(h), t[h.index(max(h))])
-    print("Vitesse max, temps et hauteur :", max(v), t[v.index(max(v))], h[v.index(max(v))])
+    print("Temps et vitesse du retour au sol :", t[-1], u[2,-1])
+    print("Hauteur max et temps :", max(u[1,:])) #, t[h.index(max(h))])
+    print("Vitesse max, temps et hauteur :", max(u[2,:])) #, t[v.index(max(v))], h[v.index(max(v))])
 
-    plt.plot(t,h)
-    plt.plot(t,v)
+    print(u)
+
+    plt.plot(t, u[0,:])
+    plt.plot(t, u[1,:])
+    plt.plot(t, u[2,:])
     plt.show()
 
